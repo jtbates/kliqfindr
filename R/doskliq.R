0c1015bb3fd49fc4081d832599d10958bd336a36
Jordan T. Bates <jtbates@gmail.com>
2019-06-23 15:03:29 -0700

    remove normalizePath, not necessary

--- R/doskliq.R
+++ R/doskliq.R
@@ -11,8 +11,8 @@ doskliq_exe_path <- function(){
 #' @param input_path The path to read the input files from
 #' @param output_path The path to write the output files to
 doskliq_run <- function(input_path, output_path){
-  cmd_path <- normalizePath(file.path(input_path, 'test.cmd'))
-  list_path <- normalizePath(file.path(input_path, 'test.list'))
+  cmd_path <- file.path(input_path, 'test.cmd')
+  list_path <- file.path(input_path, 'test.list')
 
   # execute command in the output directory
   wd <- getwd()
